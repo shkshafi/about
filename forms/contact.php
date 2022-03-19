28de81f4b7b4c466773b1ce8b94e01346f8ee16d
Shafi Ur Rahman <shaik.shafi.ur.rahman@gmail.com>
2022-03-19 23:43:31 +0530

    mmail

--- forms/contact.php
+++ forms/contact.php
@@ -7,21 +7,21 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'shaik.shafi.ur.rahman@gmail.com';
+  // $receiving_email_address = 'shaik.shafi.ur.rahman@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -53,6 +53,36 @@
 //   $contact->add_message( $_POST['message'], 'Message', 10);
 
   
+
+
+$to = 'shaik.shafi.ur.rahman@gmail.com';
+$subject = $_POST['subject'];
+$from = $_POST['email'];
+ 
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+// Compose a simple HTML email message
+$message  = $_POST['name'];
+$message .= $_POST['email'];
+// $message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
+// $message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
+// $message .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
+
+
 
 ?>
 
